fcd8ee5e084ed608da28d0fe0ed36b3de2f1736e
christoliu <chris211032@gmail.com>
2019-06-10 21:33:37 +0800

    "2019-06-10 21:33:37.258356"

--- cmd.py
+++ cmd.py
@@ -1,6 +1,6 @@
 import subprocess
 import datetime
-nowtime = datetime.datetime.now()
+nowtime = datetime.datetime.today()
 subprocess.call(['git', 'add', '.'])
 subprocess.call(['git', 'commit', '-m', '"' + str(nowtime) + '"'])
 subprocess.call(["git", "push"])
--- python_final/cmd.py
+++ python_final/cmd.py
@@ -1,8 +1,3 @@
-import subprocess
-import datetime
-nowtime = datetime.datetime.now()
-subprocess.call(['git', 'add', '.'])
-subprocess.call(['git', 'commit', '-m', '"' + str(nowtime) + '"'])
-subprocess.call(["git", "push"])
+print('success MF')
 
 
