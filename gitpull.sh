146ecae5ab46e923798403a88448f3bc89ec6bd7
christoliu <chris211032@gmail.com>
2019-06-10 23:26:00 +0800

    2019

--- gitpull.sh
+++ gitpull.sh
@@ -1,5 +1,5 @@
 cd /Users/christo/Desktop/107-2-python_final/
 git add .
-mkdir /Users/christo/Desktop/107-2-python_final/HIHI20190610
 git commit -m "2019"
+mkdir /Users/christo/Desktop/107-2-python_final/HIHI20190610
 git push
