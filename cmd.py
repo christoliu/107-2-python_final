4a8d8d8bed6005e41a9956152b037c6449c71568
christoliu <chris211032@gmail.com>
2019-06-10 22:09:56 +0800

    QQ

--- cmd.py
+++ cmd.py
@@ -3,4 +3,3 @@ import datetime
 subprocess.call(['git', 'add', '.'])
 subprocess.call(['git', 'commit', '-m', '"It\'s ' + str(datetime.datetime.now()) + '"'])
 subprocess.call(['git', 'push'])
-
---
+++ gitpull.sh
@@ -0,0 +1,3 @@
+git add .
+git commit -m "QQ"
+echo "push la"
